9838b5ce392bf460d8565b4f06f918b9eb2bd5e8
Brady Snuggs <bradyesnuggs@gmail.com>
2022-02-26 22:50:52 +0000

    initial commit with bare bones setup.

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const LandingPage = () => {
+  return (
+    <div>
+      Hello from main. <Link to="/about">Head to about</Link>
+    </div>
+  )
+}
+
+export default LandingPage
